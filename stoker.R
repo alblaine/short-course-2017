c0d5ce77671872d1e0cbc3117ea77c3159f2c0dd
alblaine <alisonblaine@icloud.com>
2017-07-18 14:55:58 -0400

    updated stoker.r

--- stoker.R
+++ stoker.R
@@ -1,18 +1,26 @@
 #install.packages("tidyverse")
 #install.packages("gutenbergr")
-#install.packages("NLP")
 #install.packages("tm")
 #install.packages("scales")
 #install.packages("stringr")
 #install.packages("tidytext")
+#install.packages("igraph")
+#install.packages("ggraph")
+#install.packages("topicmodels")
+#install.packages("NLP")
+#install.packages("openNLP")
 
 library(tidyverse)
 library(gutenbergr)
-library(NLP)
 library(tm)
 library(scales)
 library(stringr)
 library(tidytext)
+library(igraph)
+library(ggraph)
+library(topicmodels)
+library(NLP)
+library(openNLP)
 
 # 1. Run this code to get information about all poe works, filtering duplicates
 stoker <- gutenberg_works(author == "Stoker, Bram")
@@ -119,7 +127,7 @@ plot_distinct <- distinct_words %>%
 
 # 19. Plot the top 15 unique terms for Dracula 
 
-plot_dracula <- distinct_words %>%
+plot_dracula <- distinct_words %>%  # creates a variable where the plot will be stored
   filter(title =="Dracula") %>%
   mutate(word = factor(word, levels = rev(unique(word)))) %>%  # sorts tf-idf from greatest to least
   top_n(15) %>%
@@ -128,41 +136,182 @@ plot_dracula <- distinct_words %>%
   labs(x = NULL, y = "tf-idf") +
   coord_flip()
 
-# 20. Create bigrams (word pairs) for all texts
+# 20. See the plot by typing 'plot_dracula' in the console and pressing enter
+
+plot_dracula
+
+### Topic 2: N-grams / word pairs ### 
+
+# 21. Create bigrams (word pairs) for all texts
+
 bigrams <- works %>%
   unnest_tokens(bigram, text, token = "ngrams", n = 2)
 
-# finds most common bigrams by count
-bigrams %>%
-  count(bigram, sort = TRUE)
 
-# filters out stop words, strips odd characters around words, and recounts bigrams
-bigrams_separated <- poe_bigrams %>%
+# 22. Type 'bigrams' into console and hit Enter to see output
+
+
+# 23. Find most common bigrams by count
+
+bigrams %>%
+  count(bigram, sort = TRUE) # counts bigram pairs and sorts them by title 
+
+# 24. Strip non-alpha characters (punctuation and dashes) from words
+
+bigrams_separated <- bigrams %>%
   separate(bigram, c("word1", "word2"), sep = " ") %>%
-  mutate(word1 = str_extract(word1, "[a-z']+")) %>%
+  mutate(word1 = str_extract(word1, "[a-z']+")) %>% 
   mutate(word2 = str_extract(word2, "[a-z']+"))
+
+# 25. Filter out stop words from bigrams 
 
 bigrams_filtered <- bigrams_separated %>%
   filter(!word1 %in% stop_words$word) %>%
   filter(!word2 %in% stop_words$word)
 
+# 26. Recount the bigrams
+
 bigram_counts <- bigrams_filtered %>% 
   count(word1, word2, sort = TRUE)
 
-# searches for top bigrams based on input for word2, sorted by title
+
+#27. Make a network graph of the top bigrams
+
+bigram_graph <- bigram_counts %>%
+  filter(n > 20) %>%
+  graph_from_data_frame()
+
+bigram_graph
+
+set.seed(2017)  # set is a starting number used to generate a random sequence of numbers - you can
+# reproduce results if you set the seed to the same number each time. 
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link() +
+  geom_node_point() +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1)
+
+
+# 27. Search for top bigrams based on input for word2, sorted by title
 bigram_search <- bigrams_filtered %>%
-  filter(word2 == "heart") %>%
+  filter(word2 == "lucy") %>%
   count(word1, title, sort = TRUE)
 
-# searches for top bigrams overall based on word1 ("heart")
+# 28. Search for top bigrams overall based on word2 ("love")
 bigram_no_titles <- bigrams_filtered %>%
-  filter(word2 == "heart") %>%
+  filter(word2 == "love") %>%
   count(word1, sort = TRUE)
 
-# searches for a particular string in originally downloaded text (stored in 'works')
-# extracts those strings and writes them to a .CSV file on your desktop
-works %>% 
-  filter(str_detect(text, "heart")) %>% 
-  select(text, title) %>%
-  write.csv(.,file = "~/Desktop/poe.csv")
+# 29. Now search for another word in the texts by changing the word in line 175 from "love" to
+# something else. Run the code to see what happens.
 
+
+# 30. Search for a particular string ("death") in Dracula,
+# extract those strings and write them to a .CSV file on your desktop
+works %>% 
+  filter(title=="Dracula", str_detect(text, "death")) %>% 
+  select(text, title) %>%
+  write.csv(.,file = "~/text-analysis-with-R/strings.csv")
+
+
+### Topic 3: Topic Modeling Dracula ### 
+## In this exercise, we are going to do some topic modeling on the chapters of Bram Stoker's Dracula
+
+# 31. Filter 'works' to just include Dracula. 
+
+dracula <- works %>%
+  filter(title == "Dracula") 
+
+# 32. Delete title page and table of contents by filtering out all text before line 157. 
+# This step could be combined with previous step by adding a pipe operator %>% 
+
+dracula <- dracula %>%
+  mutate(linenumber = row_number()) %>%
+  filter(linenumber > 156)
+
+
+# 33. Now separate the text into chapters. This code looks for the word "Chapter" followed by Roman numerals and
+# increments a counter
+
+dracula <- dracula %>% 
+    mutate(chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", 
+                                                 ignore_case = TRUE))))
+
+
+# 34. Tokenize text
+
+chapter_words <- dracula %>%
+  unnest_tokens(word, text)
+
+
+# 35. Find word counts
+
+word_counts <- chapter_words %>%
+  anti_join(stop_words) %>%
+  count(chapter, word, sort = TRUE) %>%
+  ungroup() %>%
+  unite(document, chapter)
+
+# 36. Create a document term matrix on word counts using tidytext function 'cast_dtm()
+chapters_dtm <- word_counts %>%
+  cast_dtm(document, word, n)                                
+
+
+# 36. Run the Latent Dirichlet Allocation model on the document matrix.
+
+chapters_lda <- LDA(chapters_dtm, k = 4, control = list(seed = 1234))
+
+
+chapters_lda
+
+# 37. Create a table where each word is listed with a probability of a term being generated from the topic 
+# In this case, the probability that you'll find the term in a given chapter.
+# beta = the probability of a term generated from a topic according to the multinomial model
+
+chapter_topics <- tidy(chapters_lda, matrix = "beta")  
+chapter_topics
+
+
+# 38. Counts the top 5 terms with the highest probabilities by chapter.
+top_terms <- chapter_topics %>%
+  group_by(topic) %>%
+  top_n(5, beta) %>%  
+  ungroup() %>%
+  arrange(topic, -beta)  # arranges topics in descending order of highest-lowest beta
+
+top_terms
+
+# 39. Creates plots of topics
+
+top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +  # draws a bar chart
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +  # breaks chart into subplots, each with their own scales
+  coord_flip()
+
+# 40. The problem with the steps we just did is that the topics generated aren't useful. 
+# They're heavily weighted toward character names and include lots of common terms (time, hand)
+# Part-of-speech tagging can help us remove all non-nouns.
+
+
+##### Part of Speech Tagging - Do not run this code in workshop ########### 
+annotators <- list(sent_token = Maxent_Sent_Token_Annotator(),
+                   word_token = Maxent_Word_Token_Annotator(),
+                   pos_tag    = Maxent_POS_Tag_Annotator())
+
+tagPOS <- function(x, ann = annotators) {
+  s <- as.String(x)
+  a2 <- annotate(s, list(ann$sent_token, ann$word_token))
+  a3 <- annotate(s, ann$pos_tag, a2)
+  a3w <- subset(a3, type == "word")
+  POStags <- unlist(lapply(a3w$features, `[[`, "POS"))
+  POStagged <- paste(sprintf("%s/%s", s[a3w], POStags), collapse = " ")
+  list(POStagged = POStagged, POStags = POStags)
+}
+
+
+draculaTag <- tagPOS(dracula$text) # NOTE: tags each word with a part of speech label. Takes about 10-15 minutes to complete
+
+#write tagged text to a file
+
+###
