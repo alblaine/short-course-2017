cb8cab72c2b120024beb4f7b631fdbbf796f922d
alblaine <alisonblaine@icloud.com>
2017-08-04 15:39:57 -0400

    added more comments to script

--- sentiment.R
+++ sentiment.R
@@ -14,30 +14,32 @@ library(RSentiment)
 # 3. Read in Twitter data from a CSV file. The data is a list of tweets.
 tweets_file <- read_csv("weather-tweets.csv")
 
-# 4. Get the tweet text only from the data you loaded in
-tweets <- tweets_file$text
+# 4. Get the tweet text only from the data you loaded in. 
 
-tweets <- iconv(tweets, to="ASCII", sub="") #strips non-ASCII characters from tweets
+tweets <- tweets_file$text  # tweets_file$text means the "text" variable of the tweets_file dataset
+
+tweets <- iconv(tweets, to="ASCII", sub="") # uses iconv function (base package) to strip non-ASCII characters from tweets.
 
 tweets <- tolower(tweets) #converts tweets to lower case
 
 tweets <- gsub('[[:punct:]]', '', tweets) # takes out punctuation from the tweets
 
-sentiments <- calculate_sentiment(tweets)  #function is from the RSentiment package
+sentiments <- calculate_sentiment(tweets)  # calculate_sentiment() function is from the RSentiment package
 
-tweet_sents <- data.frame(sentiments) #makes a data frame from the tweets and sentiment ratings
+tweet_sents <- data.frame(sentiments) # makes a data frame from the tweets and sentiment ratings using data.frame() function
 
-tweet_sents  #prints out the data
+tweet_sents  # prints out the data
 
-# writes tweet_sents data frame to a file
+# Writes tweet_sents data frame to a file using write.csv() function from base R package
 
 write.csv(tweet_sents, file = "tweet-sentiments.csv")
 
-# sums sentiments by category
+# Sums sentiments by category
 
 calculate_total_presence_sentiment(tweets)
 
-# makes a histogram of the sentiment ratings
+# Makes a histogram of the sentiment ratings
+
 ggplot(tweet_sents) + geom_histogram(aes(tweet_sents$sentiment), stat="count")
 
 # the problem with the previous graph is that the X-axis categories were out of order. 
